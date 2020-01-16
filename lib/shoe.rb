3b427a52084a84b9b09851ee16988955d96535ca
Cody Toney-Griffith <cody.toney@gmail.com>
2020-01-16 05:15:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,8 +5,8 @@ attr_reader :brand
 def initialize(brand)
   @brand=brand
 end
- def cobble 
+ def cobble (condition = new)
     puts "Your shoe is as good as new!"
-    condition == new
+    
 end
 end
